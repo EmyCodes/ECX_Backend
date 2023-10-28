fbeb68dfaab7cbe6f7b78faac5c2e1ba25376193
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-10-28 22:08:45 +0100

    chore: removed unneedful package

--- Assessment_1/server.py
+++ Assessment_1/server.py
@@ -4,7 +4,7 @@ simple crud operations on a database"""
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy import Column, Integer, String, Text
 from sqlalchemy.ext.declarative import declarative_base
 # from typing import Type
 from database_info import username, password, database_name
