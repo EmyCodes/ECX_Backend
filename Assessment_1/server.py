c0080b7ff495252f04987f9cb3f01339baeedfd1
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-10-28 15:03:11 +0100

    started building

--- Assessment_1/server.py
+++ Assessment_1/server.py
@@ -1,1 +1,15 @@
 #!/bin/python3
+"""A Web server (program) that performs
+simple crud operations on a database"""
+
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# Database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+if __name__ == '__main__':
+    app.run(debug=True)
