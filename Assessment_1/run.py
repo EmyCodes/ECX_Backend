96cde6a6b07c3a4fd743e8b8d424152a034b79fd
Ogundare Olamide Emmanuel <ogundareolamideemmanuel@gmail.com>
2023-10-28 15:53:12 +0100

    fix: resolved errors while running this file

--- Assessment_1/run.py
+++ Assessment_1/run.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 '''Database initialization script'''
 
-from server import db
+from server import app, db  # Import app and db from server.py
+
+# Create the Flask app context
+app.app_context().push()
+
+# Initialize the database
 db.create_all()
